f64f4831f936c158008ee5f27c32b5f44b9d54bb
nicekid1 <86746988+nicekid1@users.noreply.github.com>
2024-11-26 22:00:58 +0330

    Set up development environment and configuration

---
+++ index.js
@@ -0,0 +1,26 @@
+
+//requires
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+//middlewares
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+//port setting
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
